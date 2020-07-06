6a5492ebd5dfec278358a6ebc8bcdc3f43e65313
JeremyPerezSwift <jeremyperez34000@gmail.com>
2020-07-06 14:20:15 +0200

    Adding the location

--- APIGoogle/ViewController.swift
+++ APIGoogle/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var locationLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getCurrentLocation()
     }
-
-
+    
+    func getCurrentLocation() {
+        // Ask for authorisation from the user
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("location = \(locValue.latitude) \(locValue.longitude)")
+        locationLabel.text = "location = \(locValue.latitude) longitude: \(locValue.longitude)"
+    }
+}
